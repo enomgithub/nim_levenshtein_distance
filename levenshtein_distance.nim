fd8b147a6fedff8ad1884743c59e7236924a1375
enom <satoshi.enom@gmail.com>
2017-12-25 21:52:02 +0900

    Modify: Bug fix and refactoring

--- levenshtein_distance.nim
+++ levenshtein_distance.nim
@@ -1,6 +1,11 @@
-proc levenshteinDistance(str1: string, str2: string): int =
-  const lenMax: int = 100
-  var d: array[lenMax, array[lenMax, int]]
+import sequtils
+import unicode
+
+
+proc levenshteinDistance( str1: string | seq[Rune]
+                        , str2: string | seq[Rune]
+                        ): int =
+  var d: seq[seq[int]] = newSeqWith(str1.len + 1, newSeq[int](str2.len + 1))
   for i in 0..str1.len:
     for j in 0..str2.len:
       d[i][j] =
@@ -10,21 +15,30 @@ proc levenshteinDistance(str1: string, str2: string): int =
   for i in 1..str1.len:
     for j in 1..str2.len:
       let cost =
-        if str1[i] == str2[j]: 0
+        if str1[i - 1] == str2[j - 1]: 0
         else: 1
-      d[i][j] = min([
-        d[i - 1][j] + 1,
-        d[i][j - 1] + 1,
-        d[i - 1][j - 1] + cost
-      ])
+      d[i][j] =
+        min( [ d[i - 1][j] + 1
+             , d[i][j - 1] + 1
+             , d[i - 1][j - 1] + cost
+             ]
+           )
   return d[str1.len][str2.len]
 
 
 proc main() =
+  # ASCII 文字列同士の比較
   const str1: string = "santa"
   const str2: string = "satan"
   echo "Levenshtein Distance between ", str1, " and ", str2,
        " is ", levenshteinDistance(str1, str2)
+  echo "(Weight: Insertion = 1, Deletion = 1, Substitution = 1)"
+
+  # Unicode 文字列同士の比較
+  const str3: string = "ニーモニック"
+  const str4: string = "デモニック"
+  echo "Levenshtein Distance between ", str3, " and ", str4,
+       " is ", levenshteinDistance(str3.toRunes, str4.toRunes)
   echo "(Weight: Insertion = 1, Deletion = 1, Substitution = 1)"
 
 
