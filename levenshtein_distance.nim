77de444f02176955a3d05e81b485ddc8ca09e3a6
enom <satoshi.enom@gmail.com>
2017-12-26 10:23:36 +0900

    Modify: Refactoring

--- levenshtein_distance.nim
+++ levenshtein_distance.nim
@@ -2,16 +2,15 @@ import sequtils
 import unicode
 
 
-proc levenshteinDistance( str1: string | seq[Rune]
-                        , str2: string | seq[Rune]
-                        ): int =
-  var distance: seq[seq[int]] = newSeqWith(str1.len + 1, newSeq[int](str2.len + 1))
+proc levenshteinDistance(str1, str2: string | seq[Rune]): int =
+  var distance: seq[seq[int]] =
+    newSeqWith( str1.len + 1
+              , newSeq[int](str2.len + 1)
+              )
   for i in 0..str1.len:
-    for j in 0..str2.len:
-      distance[i][j] =
-        if j == 0: i
-        elif i == 0: j
-        else: 0
+    distance[i][0] = i
+  for j in 1..str2.len:
+    distance[0][j] = j
   for i in 1..str1.len:
     for j in 1..str2.len:
       let cost =
@@ -26,20 +25,24 @@ proc levenshteinDistance( str1: string | seq[Rune]
   return distance[str1.len][str2.len]
 
 
-proc main() =
-  # ASCII 文字列同士の比較
-  const str1: string = "santa"
-  const str2: string = "satan"
+proc showLD(str1, str2: string | seq[Rune]) =
   echo "Levenshtein Distance between ", str1, " and ", str2,
        " is ", levenshteinDistance(str1, str2)
   echo "(Weight: Insertion = 1, Deletion = 1, Substitution = 1)"
 
+
+proc main() =
+  # ASCII 文字列同士の比較
+  const
+    str1: string = "santa"
+    str2: string = "satan"
+  showLD(str1, str2)
+
   # Unicode 文字列同士の比較
-  const str3: string = "ニーモニック"
-  const str4: string = "デモニック"
-  echo "Levenshtein Distance between ", str3, " and ", str4,
-       " is ", levenshteinDistance(str3.toRunes, str4.toRunes)
-  echo "(Weight: Insertion = 1, Deletion = 1, Substitution = 1)"
+  const
+    str3: string = "ニーモニック"
+    str4: string = "デモニック"
+  showLD(str3.toRunes, str4.toRunes)
 
 
 if isMainModule:
